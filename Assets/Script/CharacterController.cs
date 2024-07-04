768f0ccfcfd32b012c04ad4581e9d9d53deaf95f
Destroyer <2673630208@qq.com>
2024-07-04 21:03:58 +0800

    更新脚本

--- Assets/Script/CharacterController.cs
+++ Assets/Script/CharacterController.cs
@@ -1,18 +1,35 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class CharacterController : MonoBehaviour
 {
     [SerializeField] private GameObject m_SmallShinmyoumaru;
     [SerializeField] private GameObject m_HugeShinmyoumaru;
     [SerializeField] private float m_DefaultSpeed;
+    [SerializeField] private float m_SmallJumpSpeed;
+    [SerializeField] private float m_BigJumpSpeed;
 
 
     private bool m_SmallStatus = true;
+    [SerializeField] private bool m_IsJumping = false;
     private Rigidbody2D rb;
     private float horizontal;
+
+    public Vector2 bigLeftCenter;
+    public Vector2 bigRightCenter;
+    public Vector2 bigLeftSize;
+    public Vector2 bigRightSize;
+    public float m_Offset;
+    public LayerMask LayerMask;
+
+
+    public const float G = 9.8f;
+
+    private float m_CurrentVSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +38,7 @@ public class CharacterController : MonoBehaviour
         if (rb == null)
             throw new System.Exception("Rigidbody is null!");
         m_SmallStatus = true;
+        m_IsJumping = false;
         SetStyle();
     }
 
@@ -29,9 +47,16 @@ public class CharacterController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
 
+        m_IsJumping = !CheckArriveGround();
+
         if (Input.GetKeyDown(KeyCode.C))
         {
             ChangeStyle();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Jump();
         }
     }
 
@@ -39,13 +64,32 @@ public class CharacterController : MonoBehaviour
     {
         float delta_horizontal = Time.deltaTime * m_DefaultSpeed * horizontal;
 
-        Vector2 pos = rb.transform.position;
-        pos.x = pos.x + delta_horizontal;
-        rb.MovePosition(pos);
+        // Vector2 pos = rb.transform.position;
+        // pos.x = pos.x + delta_horizontal;
+        // rb.MovePosition(pos);
+        Vector2 newVelocity = rb.velocity;
+        newVelocity.x = horizontal * m_DefaultSpeed;
+        rb.velocity = newVelocity;
+    }
+
+    private void Jump()
+    {
+        if (!CheckArriveGround() || m_IsJumping)
+        {
+            return;
+        }
+
+        m_IsJumping = true;
+        rb.velocity = new Vector2(rb.velocity.x, m_SmallJumpSpeed);
     }
 
     private void ChangeStyle()
     {
+        if (!CheckCouldChange() && m_SmallStatus)
+        {
+            return;
+        }
+
         m_SmallStatus = !m_SmallStatus;
         SetStyle();
     }
@@ -62,5 +106,28 @@ public class CharacterController : MonoBehaviour
             m_SmallShinmyoumaru.SetActive(false);
             m_HugeShinmyoumaru.SetActive(true);
         }
+    }
+
+    private bool CheckCouldChange()
+    {
+        var left = Physics2D.OverlapBox(transform.position + (Vector3)bigLeftCenter, bigLeftSize, 0, LayerMask);
+        var right = Physics2D.OverlapBox(transform.position + (Vector3)bigRightCenter, bigRightSize, 0, LayerMask);
+        return !(left && right);
+    }
+
+    private bool CheckArriveGround()
+    {
+        return Physics2D.Raycast(transform.position, Vector2.down, m_Offset, LayerMask);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position + (Vector3)bigLeftCenter, bigLeftSize);
+        Gizmos.DrawWireCube(transform.position + (Vector3)bigRightCenter, bigRightSize);
+        Handles.Label(
+            transform.position + new Vector3(-0.25f, 1f),
+            $"{m_IsJumping}"
+        );
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * m_Offset);
     }
 }
